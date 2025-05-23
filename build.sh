7144ee8303b95352457aab969425f93b6d8c3eca
Andry Becerra <andryjbv@gmail.com>
2025-05-22 23:15:44 -0400

    Implement build script to install dependencies

--- build.sh
+++ build.sh
@@ -7,16 +7,21 @@ set -e
 ###############################################
 # PROJECT DEPENDENCIES AND CONFIGURATION
 ###############################################
-# TODO: Install project dependencies if needed based on relevant config/lock files in the repo.
+# Install project dependencies if needed based on relevant config/lock files in the repo.
 # Note that we are developing the project, even if dependencies have been installed before, we need to install again to accommodate the changes we made.
-# <dependency-installation-commands>
-# TODO: Configure project and environment variables
-# <config-commands>
+python -m pip install --upgrade pip
+# install runtime and test dependencies
+python -m pip install -r requirements.txt -r test_requirements.txt
+# install the package in editable mode
+python -m pip install -e .
+# Configure project and environment variables
+export DJANGO_SETTINGS_MODULE=tests.settings
 
 ###############################################
 # BUILD
 ###############################################
 echo "================= 0909 BUILD START 0909 ================="
-# TODO: Build the project if needed. Note that we are developing the project and making changes to it, even if it has been build before, we need to build it again.
-# <build-commands>
+# No additional build steps required for this project
+
 echo "================= 0909 BUILD END 0909 ================="
+
